16875c80222df80764082dd29a836be9be1bcb8b
AngeloZBA <abarbaa@est.ups.edu.ec>
2024-02-16 16:16:49 -0500

    Add(III): Edit hello service - lastName Param as Optional

--- src/main/java/com/ups/poo/controller/TestController.java
+++ src/main/java/com/ups/poo/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Hello world, this is my first Project!....";
+        return "Hello teacher, this is my Project!....";
     }
 
     @GetMapping("/hello")
@@ -26,12 +26,42 @@ public class TestController {
         }
         return message;
     }
+    @GetMapping("/my-information")
+    public String myInformation(@RequestParam String name,
+                        @RequestParam(required = false) String lastname,
+                        @RequestParam(required = false) Integer Age) {
+        String message = "Hello teacher, this is my first SpringBootProject!" +
+                ", and my name is: " + name;
+        if (lastname != null){
+            message =  message + " " + lastname;
+        }
+        if (Age != null) {
+            message = message + " and my age is " + Age;
+        }
+        return message;
+    }
 
+    /*
     @GetMapping("/concat/{name}/{lastname}/{age}")
     public String concatenate(@PathVariable String name,
                               @PathVariable String lastname,
                               @PathVariable int age) {
         return "This is my second rest service!, and my name is "
                 + name + " " + lastname + " and my age is: " + age;
+    }
+
+     */
+    @GetMapping("/concat1/{name}/{lastname}/{age}")
+    public String concatenate1(@PathVariable String name,
+                              @PathVariable(required = false) String lastname,
+                              @PathVariable(required = false) Integer age) {
+        String message = "This is my frist SpringBootProyect and my name is: " + name;
+        if (lastname != null){
+             message =  message + ", my last name is " + lastname;
+        }
+        if (age != null) {
+            message = message + " and my age is " + age;
+        }
+        return message;
     }
 }
